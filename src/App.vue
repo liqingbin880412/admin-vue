2e568d422bdee5a806244549340320cee018a782
liqingbin <lqb@com>
2018-04-11 09:10:44 +0800

    :用户注册登录

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-   <p>app component</p>
+
+    <router-view></router-view>
   </div>
 </template>
 
@@ -11,5 +12,4 @@ export default {
 </script>
 
 <style>
-
 </style>
---
+++ src/components/home/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>home componeent</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
